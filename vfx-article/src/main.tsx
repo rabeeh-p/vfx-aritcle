1d12fb140538690eba6fff23a3df937119b90255
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-10 20:33:38 +0530

    updated

--- vfx-article/src/main.tsx
+++ vfx-article/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import './index.css';
 
+// MAIN COMPONENT
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
