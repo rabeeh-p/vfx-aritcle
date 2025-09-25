9a569720ebeb0cf3486f69c3bcbfd8a28ecab1d2
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-25 23:46:16 +0530

    code cleaned

--- vfx-article/src/pages/Home.tsx
+++ vfx-article/src/pages/Home.tsx
@@ -139,8 +139,6 @@ const Home: React.FC = () => {
       </section>
 
 
-
-
       {/* Stats Section */}
       <section className="py-24 px-6 bg-gray-800/50">
         <div className="max-w-7xl mx-auto">
