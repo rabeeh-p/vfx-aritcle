c05201f8a4ad77845b09a79ca745f38c3ae03ed1
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-28 23:00:25 +0530

    refactor : code change to tsx to jsx

--- vfx-article/src/components/Layout.jsx
+++ vfx-article/src/components/Layout.jsx
@@ -4,6 +4,7 @@ import Sidebar from './Sidebar';
 import Header from './Header';
 
 const Layout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="flex h-screen bg-gray-900">
