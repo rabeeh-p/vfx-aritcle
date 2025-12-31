5cd7bac8d242dc386859633dedcad185af84f437
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-12-31 23:30:35 +0530

    syle : update

--- vfx-article/src/App.jsx
+++ vfx-article/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-900 text-white">
-        <Layout>
+        <Layout>44tel
           <Routes>
             {/* HOME PAGE */}
             <Route path="/" element={<Home />} />
